57b55b1dedc6d4de67064f72d22f872f28706f77
Handy Milliance <milliance23@gmail.com>
2022-07-17 17:04:38 -0300

    Add...

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,10 +3,7 @@ const productController = require('../controllers/productController')
 
 const router= express.Router()
 
-
 router.route('/').get(productController.all).post(productController.create)
-
-
 
 router.route('/:id').get(productController.one).put(productController.update).delete(productController.delete)
 
